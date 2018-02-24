a475c5128687d97923e05934e3799462ee2ee3d4
Scott <wse632now@gmail.com>
2018-02-24 16:27:18 -0600

    JavaScript Organization - P1 - OOP - completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,18 +1,43 @@
 // console.log("Hello! My name is John Doe, and my favorite color is blue!");
 // console.log("Hello! My name is Jane Smith, and my favorite color is green!");
 
-function person(name, favColor) {
-    console.log("Hello! My name is " + name + ", and my favorite color is " + favColor + "!");
-};
+// person(john.name, john.favColor);
+// person("Jane Smith", "green");
 
 // var johnName = "John Doe";
 // var johnFavColor = "blue";
 
-var john = {
-    name: "John Buck",
-    favColor: "camo"
+// console.log("Hello! My name is " + name + ", and my favorite color is " + favColor + "!");
 
-};
+// var john = {
+//     name: "John Buck",
+//     favColor: "camo",
+//     greet: function() {
+//         console.log("Hello! My name is " + john.name + ", and my favorite color is " + john.favColor + ".");
+//     }
+// };
 
-person(john.name, john.favColor);
-person("Jane Smith", "green");
+// var jane = {
+//     name: "Jane Doe",
+//     favColor: "headlights",
+//     greet: function() {
+//         console.log("Hello! My name is " + jane.name + ", and my favorite color is " + jane.favColor + ".");
+//     }
+// };
+
+// john.greet();
+// jane.greet();
+
+function Person(fullName, favColor) {
+    this.name = fullName;
+    this.favoriteColor = favColor;
+    this.greet = function() {
+        console.log("Hello there! My name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");        
+    }
+}
+
+var john = new Person("John Buck", "camo");
+john.greet();
+var jane = new Person("Jane Doe", "headlights");
+jane.greet();
+

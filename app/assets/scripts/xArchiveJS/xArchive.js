720543784780ccae1bdee3c248ca76dfb9be6b37
Scott <wse632now@gmail.com>
2018-02-25 16:35:39 -0600

    JavaScript Organization - P2 - JS Modules & Webpack - started - DFW

---
+++ app/assets/scripts/xArchiveJS/xArchive.js
@@ -0,0 +1,29 @@
+// console.log("Hello! My name is John Doe, and my favorite color is blue!");
+// console.log("Hello! My name is Jane Smith, and my favorite color is green!");
+
+// person(john.name, john.favColor);
+// person("Jane Smith", "green");
+
+// var johnName = "John Doe";
+// var johnFavColor = "blue";
+
+// console.log("Hello! My name is " + name + ", and my favorite color is " + favColor + "!");
+
+// var john = {
+//     name: "John Buck",
+//     favColor: "camo",
+//     greet: function() {
+//         console.log("Hello! My name is " + john.name + ", and my favorite color is " + john.favColor + ".");
+//     }
+// };
+
+// var jane = {
+//     name: "Jane Doe",
+//     favColor: "headlights",
+//     greet: function() {
+//         console.log("Hello! My name is " + jane.name + ", and my favorite color is " + jane.favColor + ".");
+//     }
+// };
+
+// john.greet();
+// jane.greet();

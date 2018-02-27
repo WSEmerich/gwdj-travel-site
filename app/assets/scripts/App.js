98de40116dd9ac3e8e7847b4eca918672ca6dc21
Scott <wse632now@gmail.com>
2018-02-26 18:06:33 -0600

    JS organize - Ps - Integrate Webpack into Gulp auto - started

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
+var $ = require('jquery');
 var Person = require("./modules/Person.js");
 
 var john = new Person("John Buck", "camo");
 john.greet();
-var jane = new Person("Jane Doe", "headlights");
+var jane = new Person("Jane Doe", "also camo");
 jane.greet();
 
+$("h1").remove();

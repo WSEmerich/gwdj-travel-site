d56d5a4dea931a54de35322db5071ee95cca05b3
Scott <wse632now@gmail.com>
2018-03-01 16:12:44 -0600

    JS Organize - P4 - Babel - integrate Babel

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,9 +3,7 @@ var Person = require("./modules/Person.js");
 
 
 
-
-
 var john = new Person("John Buck", "camo");
 john.greet();
-var jane = new Person("Jane Doe", "anything but camo");
+var jane = new Person("Jane Doe", "headlights!");
 jane.greet();
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,25 @@ var config = {
     output: {
         path: path.resolve(__dirname, "./app/temp/scripts"),
         filename: "app.bundle.js"
+    },
+    // module: {
+    //     rules: [
+    //       {
+    //         test: /\.js$/,
+    //         exclude: /(node_modules|bower_components)/,
+    //         use: {
+    //           loader: 'babel-loader',
+    //           options: {
+    //             presets: ['@babel/preset-env']
+    //           }
+    //         }
+    //       }
+    //     ]
+    //   }
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+        ]
     }
 };
 

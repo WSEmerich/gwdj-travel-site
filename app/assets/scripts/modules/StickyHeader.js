efe20223aff7e7a4c8d6c6b9e8fe6c5f7a43f924
Scott <wse632now@gmail.com>
2018-03-04 22:57:53 -0600

    Desktop Sticky Header - P2 - hilite current section link

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -6,7 +6,11 @@ class StickyHeader {
     this.siteHeader = $(".site-header");
     this.headerTriggerElement = $(".large-hero__title");
     this.createHeaderWaypoint();
+    this.pageSections = $(".page-section");
+    this.headerLinks = $(".primary-nav a");
+    this.createPageSectionWaypoints();
   }
+
 
   createHeaderWaypoint() {
     var that = this;
@@ -19,6 +23,35 @@ class StickyHeader {
           that.siteHeader.removeClass("site-header--dark");
         }
       }
+    });
+  }
+
+  createPageSectionWaypoints() {
+    var that = this;
+    this.pageSections.each(function() {
+      var currentPageSection = this;
+      new Waypoint({
+        element: currentPageSection,
+        handler: function(direction) {
+          if(direction == "down") {
+            var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+            that.headerLinks.removeClass("is-current-link");
+            $(matchingHeaderLink).addClass("is-current-link");
+          }          
+        },
+        offset: "20%"
+      });
+      new Waypoint({
+        element: currentPageSection,
+        handler: function(direction) {
+          if(direction == "up") {
+            var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+            that.headerLinks.removeClass("is-current-link");
+            $(matchingHeaderLink).addClass("is-current-link");
+          }          
+        },
+        offset: "-40%"
+      });
     });
   }
 }

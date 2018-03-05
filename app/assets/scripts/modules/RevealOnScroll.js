de4edc3dbcff12b2174f0a7217d2b6d731fe2bc6
Scott <wse632now@gmail.com>
2018-03-04 19:22:27 -0600

    Reveal-On-Scroll - P1 - install and "connect" Waypoints

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,14 +1,23 @@
 import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 
 class RevealOnScroll {
   constructor() {
     this.itemsToReveal = $(".feature-item");
     this.hideInitially();
+    this.createWaypoints();
   }
 
   hideInitially() {
     this.itemsToReveal.addClass("reveal-item");
   }
+
+  createWaypoints() {
+    this.itemsToReveal.each(function() {
+      alert("Testing!!");
+    });
+  }
 }
+
 
 export default RevealOnScroll;

34d4ee79c031720aa138de5e44129bdf73409118
Scott <wse632now@gmail.com>
2018-03-07 21:57:56 -0600

    Build & Deploy - build task completed w/previewDist

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -2,12 +2,39 @@ var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var del = require('del');
 var usemin = require('gulp-usemin');
+var rev = require('gulp-rev');
+var cssnano = require('gulp-cssnano');
+var uglify = require('gulp-uglify');
+browserSync = require('browser-sync').create();
+
+gulp.task('previewDist', function() {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "dist"
+    }
+  });
+});
 
 gulp.task('deleteDistFolder', function() {
   return del("./dist");
 });
 
-gulp.task('optimizeImages', ['deleteDistFolder'], function() {
+gulp.task('copyGeneralFiles', ['deleteDistFolder'], function() {
+  var pathsToCopy = [
+    './app/**/*',
+    '!./app/*.html',
+    '!./app/assets/images/**',
+    '!./app/assets/styles/**',
+    '!./app/assets/scripts/**',
+    '!./app/temp',
+    '!./app/temp/**'
+  ]
+  return gulp.src(pathsToCopy)
+    .pipe(gulp.dest("./dist"))
+})
+
+gulp.task('optimizeImages', ['deleteDistFolder', 'icons'], function() {
   return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*'])
   .pipe(imagemin({
     progressive: true,
@@ -17,10 +44,13 @@ gulp.task('optimizeImages', ['deleteDistFolder'], function() {
   .pipe(gulp.dest("./dist/assets/images"));
 });
 
-gulp.task('usemin', ['deleteDistFolder'], function() {
+gulp.task('usemin', ['deleteDistFolder', 'styles', 'scripts'], function() {
   return gulp.src("./app/index.html")
-    .pipe(usemin())
+    .pipe(usemin({
+      css: [function() {return rev()}, function() {return cssnano()}],
+      js: [function() {return rev()}, function() {return uglify()}]
+    }))
     .pipe(gulp.dest("./dist"));
 });
 
-gulp.task('build', ['deleteDistFolder', 'optimizeImages', 'usemin']);
+gulp.task('build', ['deleteDistFolder', 'copyGeneralFiles', 'optimizeImages', 'usemin']);
